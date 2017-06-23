137981597bf5ba74f316736a9b15c849f54efdcd
Ilker Güller <Sly777@users.noreply.github.com>
2017-06-23 13:02:24 +0200

    Update routes.js
    Added description for using Routes

--- routes.js
+++ routes.js
@@ -1,5 +1,13 @@
 const UrlPrettifier = require('next-url-prettifier').default;
 
+//
+// Because of awesome Next.js, You don't need to add routes for all pages.
+// Every file on Pages folder basically has route as they named.
+// (index.js => /, about.js => /about, ...etc.)
+//
+// If you want to change url (for SEO or put different path), please add your route below.
+//
+
 const routes = [];
 
 routes.push({
