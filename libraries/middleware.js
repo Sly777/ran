af8d1e634868bb5c7da4a4a00a8011148ef7bcde
Sly777 <ilker.guller.777@gmail.com>
2017-06-24 15:00:38 +0200

    Fixed Redux devtools bug

--- libraries/middleware.js
+++ libraries/middleware.js
@@ -3,7 +3,7 @@ import { composeWithDevTools } from 'remote-redux-devtools';
 
 export default function createMiddleware(clientMiddleware) {
   const middleware = applyMiddleware(clientMiddleware);
-  const composeEnhancers = composeWithDevTools({ port: 8000 });
+  const composeEnhancers = composeWithDevTools({});
 
   return composeEnhancers(middleware);
 }
