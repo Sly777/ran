303504bfcb33d86c3365ae59621f1e685bf4417f
Sly777 <ilker.guller.777@gmail.com>
2017-08-07 17:26:30 +0200

    0.3.1 -> 0.4.0

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="robots" content="index,follow" />
           <meta httpEquiv="expires" content="10800" />
-          <meta name="generator" content="RAN! 0.3.1" />
+          <meta name="generator" content="RAN! 0.4.0" />
           {this.helmetHeadComponents()}
           {styleTags}
         </Head>
