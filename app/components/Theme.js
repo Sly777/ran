3a4467a9b86258c98d128af8c517ebfaa091f2fe
BjornMelgaard <melgaardbjorn@gmail.com>
2017-08-13 14:42:01 +0300

    stylefmt bug

--- app/components/Theme.js
+++ app/components/Theme.js
@@ -1,29 +1,29 @@
 import styled from 'styled-components'
 
 export const App = styled.div`
-  background-color: ${props => props.theme.colors.background};
-  color: ${props => props.theme.colors.text};
+  background-color: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.text};
 `
 
-export const A = styled.a`color: ${props => props.theme.colors.main};`
+export const A = styled.a`color: ${({ theme }) => theme.colors.main};`
 
 export const P = styled.p`
-  font-size: ${props => props.theme.font.sizes.normal};
-  line-height: ${props => props.theme.font.sizes.bigger};
+  font-size: ${({ theme }) => theme.font.sizes.normal};
+  line-height: ${({ theme }) => theme.font.sizes.bigger};
 `
 
 export const Article = styled.article`
-  margin: ${props => props.theme.alignment.horizontalcenter};
+  margin: ${({ theme }) => theme.alignment.horizontalcenter};
   max-width: 650px;
 `
 
 export const Button = styled.button`
   align-items: center;
-  background-color: ${props => props.theme.colors.main};
+  background-color: ${({ theme }) => theme.colors.main};
   border: 0;
-  color: ${props => props.theme.colors.textAlt};
+  color: ${({ theme }) => theme.colors.textAlt};
   display: flex;
-  padding: ${props => props.theme.spacing.smaller};
+  padding: ${({ theme }) => theme.spacing.smaller};
   &:active {
     background-color: ${({ theme }) =>
       theme.helper(theme.colors.main).darken(0.2).string()};
