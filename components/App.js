45c1162f445eb766fe81db9b85364196a9c4957f
Sly777 <ilker.guller.777@gmail.com>
2017-08-15 12:16:04 +0200

    updated for heroku

--- components/App.js
+++ components/App.js
@@ -1,8 +1,17 @@
 import PropTypes from 'prop-types';
 import { ThemeProvider, injectGlobal } from 'styled-components';
 import color from 'color';
+import ReactGA from 'react-ga';
 import themeList from '../libraries/theme';
 import { App as ThemedApp } from '../components/Theme';
+
+if (process.browser && !window.reactGaInit) {
+  ReactGA.initialize('UA-104637848-1');
+  window.reactGaInit = true;
+} else if (process.browser && window.reactGaInit) {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 let offlineInstalled = false;
 
