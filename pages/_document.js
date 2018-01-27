34064a3aa13cc6b1b3fd2110072e49e6630b83eb
Ilker Güller <ilker.guller@hungryhouse.com>
2018-01-27 16:28:36 +0100

    Updated styled-components SSR way

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,16 @@ import { ServerStyleSheet } from 'styled-components';
 import AppIcons from '../components/AppIcons';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(...args) {
-    const documentProps = await super.getInitialProps(...args);
+  static async getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+
     // see https://github.com/nfl/react-helmet#server-usage for more information
     // 'head' was occupied by 'renderPage().head', we cannot use it
-    return { ...documentProps, helmet: Helmet.rewind() };
+    return { ...page, styleTags, helmet: Helmet.rewind() };
   }
 
   // should render on <html>
@@ -31,9 +36,6 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    const sheet = new ServerStyleSheet();
-    const main = sheet.collectStyles(<Main />);
-    const styleTags = sheet.getStyleElement();
     return (
       <html lang="en" {...this.helmetHtmlAttrComponents()}>
         <Head>
@@ -42,10 +44,10 @@ export default class MyDocument extends Document {
           <meta name="generator" content="RAN! 0.8.0" />
           {this.helmetHeadComponents()}
           {AppIcons()}
-          {styleTags}
+          {this.props.styleTags}
         </Head>
         <body>
-          <div className="root">{main}</div>
+          <Main />
           <NextScript />
         </body>
       </html>
